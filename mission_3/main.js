8ed26cfb3dae454e13ac351f519b6be63b52a3e9
intwocave <intwocave@gmail.com>
2025-08-01 15:07:43 +0900

    [feat] implemented product patch

--- mission_3/main.js
+++ mission_3/main.js
@@ -55,7 +55,7 @@ app.post('/product', async (req, res) => {
 app.get('/product/:id', async (req, res) => {
   const { id } = req.params;
   if (!id)
-    return res.status(400).json({ "message": "Invalid parameter 'id'" });
+    return res.status(400).json({ message: "Invalid parameter 'id'" });
 
   try {
     const product = await prisma.product.findUnique({
@@ -68,6 +68,47 @@ app.get('/product/:id', async (req, res) => {
       res.status(200).json(product);
     else 
       res.status(404).json({ message: `Cannot find product with ID ${id}` })
+  } catch (err) {
+    console.error('An error has occurred: ', err.message);
+
+    res.status(500).json({ message: "An error has occurred during processing sql" });
+  }
+});
+
+app.patch('/product/:id', async (req, res) => {
+  const { id } = req.params;
+  if (!id)
+    return res.status(400).json({ message: "Invalid parameter 'id'" });
+
+  // Columns in model Product
+  const productCols = [
+    "name",
+    "description",
+    "price",
+    "tags"
+  ];
+
+  const filteredBody = Object.entries(req.body)
+    .filter(e => productCols.includes(e[0]))
+    .reduce((obj, ele) => {
+      obj[ele[0]] = ele[1]
+      return obj;
+    }, {});
+  
+  try {
+    const product = await prisma.product.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        ...filteredBody
+      }
+    });
+
+    if (product)
+      res.status(200).json(product);
+    else 
+      res.status(404).json({ message: `Cannot find product with ID ${id}` });
   } catch (err) {
     console.error('An error has occurred: ', err.message);
 
