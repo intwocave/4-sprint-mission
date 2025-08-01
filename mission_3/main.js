2456f8be9021f5f8d649d24f12cd35dd88abc0f8
intwocave <intwocave@gmail.com>
2025-08-01 11:15:45 +0900

    [feat] implemented product uploading feature

--- mission_3/main.js
+++ mission_3/main.js
@@ -8,4 +8,40 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.post('/product', async (req, res) => {
+
+  // destructuring field data from request body
+  const {
+    name,
+    description,
+    price,
+    tags
+  } = req.body;
+
+  // validation logic (possible improvements with Zod libraries in the future)
+  if ( !name || !description || !price || !tags )
+    return res.status(500).json({ message: "Invalid SQL Parameters" });
+
+  try {
+    // insert into Product table
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        tags
+      }
+    });
+
+    // send 201 response status code indicating the query was processed successfully
+    // with the Product record
+    res.status(201).json(product);
+  } catch (err) {
+    console.error('An error has occurred: ', err);
+
+    res.status(500).json({ "message": "an error has occurred during processing sql" });
+  }
+
+});
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}..`));
