3c875d8f84704c744d57728f3eba5eb7362d8440
intwocave <intwocave@gmail.com>
2025-08-01 17:29:14 +0900

    [feat] add function inquiring a certain article

--- mission_3/routes/articles.js
+++ mission_3/routes/articles.js
@@ -35,4 +35,31 @@ router.route('/')
     }
   });
 
+
+router.route('/:id')
+
+  // Get informations of a certain article
+  .get(async (req, res) => {
+    const { id } = req.params;
+    if (!id)
+      return res.status(400).json({ message: "Invalid parameter 'id'" });
+
+    try {
+      const article = await prisma.article.findUnique({
+        where: {
+          id: parseInt(id)
+        }
+      });
+
+      if (article)
+        res.status(200).json(article);
+      else 
+        res.status(404).json({ message: `Cannot find product with ID ${id}` })
+    } catch (err) {
+      console.error('An error has occurred: ', err.message);
+
+      res.status(500).json({ message: "An error has occurred during processing sql" });
+    }
+  })
+
 export default router;
