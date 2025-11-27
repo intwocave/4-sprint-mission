ceb5c70fe5c817ae094533dfa79615534da0c965
intwocave <intwocave@gmail.com>
2025-11-27 19:09:51 +0900

    feat: production 환경에서 rds 사용

--- mission_10/src/lib/prisma.ts
+++ mission_10/src/lib/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url:
+        process.env.NODE_ENV === 'production'
+          ? process.env.PRODUCTION_DATABASE_URL
+          : process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
